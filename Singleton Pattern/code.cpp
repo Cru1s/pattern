3eb326df2595d19d14d471b909c4ad051d5d0822
Cru1s <94373117+Cru1s@users.noreply.github.com>
2023-04-24 22:50:36 +0700

    Update code.cpp

--- Singleton Pattern/code.cpp
+++ Singleton Pattern/code.cpp
@@ -1,81 +1,97 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
-
-class ChocolateBoiler {
- private:
-  bool empty;
-  bool boiled;
-  //singleton
-  static ChocolateBoiler* uniqueInstance;
-  ChocolateBoiler() {
-    empty = true;
-    boiled = false;
-  }
-  //singleton
- public:
-  static ChocolateBoiler* getInstance() {
-    if (uniqueInstance == nullptr) {
-      uniqueInstance = new ChocolateBoiler();
-    }
-    return uniqueInstance;
-  }
-void fill() {
-    if (isEmpty()) {
-        empty = false;
+class ChocolateBoiler
+{
+private:
+    bool empty;
+    bool boiled;
+    // singleton
+    static ChocolateBoiler *uniqueInstance;
+    ChocolateBoiler()
+    {
+        empty = true;
         boiled = false;
-        // fill the boiler with a milk/chocolate mixture
-        std::cout << "Filled the boiler with milk/chocolate mixture." << std::endl;
-        std::cout << "Boiler is empty: " << isEmpty() << std::endl;
-        std::cout << "Boiler is boiled: " << isBoiled() << std::endl;
     }
-}
-  void drain() {
-    if (!isEmpty() && isBoiled()) {
-      // drain the boiled milk and chocolate
-      empty = true;
+    // singleton
+public:
+    static ChocolateBoiler *getInstance()
+    {
+        if (uniqueInstance == nullptr)
+        {
+            uniqueInstance = new ChocolateBoiler();
+        }
+        return uniqueInstance;
     }
-  }
-  void boil() {
-    if (!isEmpty() && !isBoiled()) {
-      // bring the contents to a boil
-      boiled = true;
+    void fill()
+    {
+        if (isEmpty())
+        {
+            empty = false;
+            boiled = false;
+            // fill the boiler with a milk/chocolate mixture
+            cout << "Filled the boiler with milk/chocolate mixture." << endl;
+            cout << "Boiler is empty: " << isEmpty() << endl;
+            cout << "Boiler is boiled: " << isBoiled() << endl;
+        }
     }
-  }
-  bool isEmpty() {
-    return empty;
-  }
-  bool isBoiled() {
-    return boiled;
-  }
+    void drain()
+    {
+        if (!isEmpty() && isBoiled())
+        {
+            // drain the boiled milk and chocolate
+            empty = true;
+        }
+    }
+    void boil()
+    {
+        if (!isEmpty() && !isBoiled())
+        {
+            // bring the contents to a boil
+            boiled = true;
+        }
+    }
+    bool isEmpty()
+    {
+        return empty;
+    }
+    bool isBoiled()
+    {
+        return boiled;
+    }
 };
 
-//singleton
-ChocolateBoiler* ChocolateBoiler::uniqueInstance = nullptr;
+// singleton
+ChocolateBoiler *ChocolateBoiler::uniqueInstance = nullptr;
 
-int main() {
-  // Get the unique instance of the chocolate boiler
-  ChocolateBoiler* boiler = ChocolateBoiler::getInstance();
+int main()
+{
+    // Get the unique instance of the chocolate boiler
+    ChocolateBoiler *boiler = ChocolateBoiler::getInstance();
 
-  // Fill the boiler
-  boiler->fill();
+    // Fill the boiler
+    boiler->fill();
 
-  // Boil the contents
-  boiler->boil();
+    // Boil the contents
+    boiler->boil();
 
-  // Drain the contents
-  boiler->drain();
+    // Drain the contents
+    boiler->drain();
 
-  // Check if the boiler is empty and boiled(used test)
-  bool empty = boiler->isEmpty();
-  bool boiled = boiler->isBoiled();
-  if (empty && !boiled ) {
-    std::cout << "The chocolate boiler is now empty and not boiled." << std::endl;
-  } else {
-    std::cout << "The chocolate boiler is not in the expected state." << std::endl;
-  }
-  // Destroy the unique instance of the chocolate boiler
-  delete boiler;
+    // Check if the boiler is empty and boiled(used test)
+    bool empty = boiler->isEmpty();
+    bool boiled = boiler->isBoiled();
+    if (empty && !boiled)
+    {
+        cout << "The chocolate boiler is now empty and not boiled." << endl;
+    }
+    else
+    {
+        cout << "The chocolate boiler is not in the expected state." << endl;
+    }
+    // Destroy the unique instance of the chocolate boiler
+    delete boiler;
 
-  return 0;
+    return 0;
+}
